740183991c56b28b5fd04b5c840b3b9161d32480
Yuval Saraf <unimonkiez@gmail.com>
2025-12-28 22:11:08 +0200

    Fixed isMain logic to support old node versions

--- src/mod.ts
+++ src/mod.ts
@@ -1,7 +1,12 @@
 export { linkAssets } from "./main.ts";
 import { runCli } from "./cli.ts";
+import { pathToFileURL } from "node:url";
+import process from "node:process";
 
-if (import.meta.main) {
+const isMain =
+  pathToFileURL(Deno.realPathSync(process.argv[1])).href === import.meta.url;
+
+if (isMain) {
   runCli().catch((e) => {
     console.error("Error running CLI:", e);
     Deno.exit(1);
